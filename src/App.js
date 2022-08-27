1647e5f1cdab87a182a467079494e7ebc444a0f0
Nikhil Limbachiya <nikhil.limbachiya@engrossinfotech.com>
2022-08-27 12:33:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import LogIn from './pages/login.jsx';
+
+export default function App() {
+  return (
+    <div>
+      <LogIn></LogIn>
+    </div>
+  )
+}
