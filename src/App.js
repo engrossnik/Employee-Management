c69e9fe96266c10dff28278950e8b668554b0cca
Nikhil Limbachiya <nikhil.limbachiya@engrossinfotech.com>
2022-08-27 12:52:47 +0530

    Created Dashboard & Login Screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import LogIn from './pages/login.jsx';
+import Dashboard from './pages/dashboard';
 
 export default function App() {
   return (
     <div>
-      <LogIn></LogIn>
+      {/* <LogIn></LogIn> */}
+      <Dashboard></Dashboard>
     </div>
   )
 }
