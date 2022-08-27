27c1db67c4891044e208b1336001dd0515ce8506
Nikhil Limbachiya <nikhil.limbachiya@engrossinfotech.com>
2022-08-27 16:38:02 +0530

    Changes

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -212,9 +212,12 @@ export default function Example() {
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
-                <div className="py-4">
-                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-                </div>
+                
+                
+
+
+
+
                 {/* /End replace */}
               </div>
             </div>
@@ -224,3 +227,5 @@ export default function Example() {
     </>
   )
 }
+
+
--- src/routes/allRoutes.jsx
+++ src/routes/allRoutes.jsx
@@ -5,9 +5,11 @@ import Login from "../pages/Authentication/Login"
 import Logout from "../pages/Authentication/Logout"
 // Dashboard
 import Dashboard from "../pages/Dashboard/index"
+import File from "../pages/Dashboard/content"
 
 const userRoutes = [
     { path: "/dashboard", component: Dashboard },
+    { path: "/file", component: File },
     // this route should be at the end of all other routes
     { path: "/", exact: true, component: () => <Redirect to="/dashboard" /> },
   ]
